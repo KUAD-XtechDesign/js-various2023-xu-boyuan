493123729bc883e74fc141dc02b4a839c3a986e6
xu-boyuan <b-xu6276@st.kyoto-art.ac.jp>
2023-10-27 16:59:21 +0900

    index 0.1

---
+++ humbuger/window01/wandow01.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    var showModalTimes = 0;  
+
+    function shouldShowModal() {
+        console.log("Referrer:", document.referrer);  // text
+        console.log("6")
+
+        var noShowUrls = [
+            "./Culture and Impact.html", 
+            "./History and Origins.html",
+            "./Preparation and Ingredients.html",
+            "./Tasting and Evaluation.html"
+        ];
+        if (noShowUrls.includes(document.referrer)) {
+            $('#pageContent').removeClass('d-none');  
+            return false;  
+        }
+        return true;  
+    }
+
+    function showModal() {
+        if (!shouldShowModal()) return;
+
+        showModalTimes++; 
+        
+        if (showModalTimes === 4) {
+            $('.modal-body').addClass('d-none');
+            $('#modalBodyText2').removeClass('d-none');
+        }
+        $('#confirmationModal').modal('show'); 
+    }
+
+    showModal(); 
+
+    $('#YES').on('click', function() {
+        $('#confirmationModal').modal('hide');  
+        $('#pageContent').removeClass('d-none');  
+    });
+
+    $('#NO').on('click', function() {
+        $('#confirmationModal').modal('hide');  
+        setTimeout(function() { 
+            if (showModalTimes < 4) {
+                showModal();  // If the display count is less than 4 times, re-display the modal dialog.
+            } else {
+                showModal();  
+                window.location.href = './page2.html';  
+            }
+        }, 500);  
+    });
+});
+
+
+
